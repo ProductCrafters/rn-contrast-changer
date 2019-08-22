cb7f852a82215f3633ce4b77d30a8fd0ab3c42e4
AlexMischenko <god.hates.us.all2001@gmail.com>
2019-08-22 16:38:11 +0300

    testing App.js

--- App.js
+++ App.js
@@ -6,26 +6,43 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import OpenCV from './NativeModules/OpenCV'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+})
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
+  checkForBlurryImage(imageAsBase64) {
+    console.log('TCL: App -> checkForBlurryImage -> OpenCV: ', OpenCV)
+
+    return new Promise((resolve, reject) => {
+      OpenCV.checkForBlurryImage(imageAsBase64, (error, dataArray) => {
+        resolve(dataArray[0])
+      })
+    })
+  }
+
+  testMethod = () => {
+    this.checkForBlurryImage(`qwer`).then(data => {
+      console.log('TCL: App -> testMethod -> data', data)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <TouchableOpacity onPress={this.testMethod}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+        </TouchableOpacity>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -46,4 +63,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
