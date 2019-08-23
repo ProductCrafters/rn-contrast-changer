9c6cad0877b43d28414dc454e2fafbeae4e24b30
AlexMischenko <god.hates.us.all2001@gmail.com>
2019-08-23 15:59:34 +0300

    removed some extra native methods -changed test image in App component;

--- ios/OpenCV/RNOpenCvLibrary.mm
+++ ios/OpenCV/RNOpenCvLibrary.mm
@@ -9,18 +9,6 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(checkForBlurryImage:(NSString *)imageAsBase64 callback:(RCTResponseSenderBlock)callback) {
-  UIImage* image = [self decodeBase64ToImage:imageAsBase64];
-  BOOL isImageBlurryResult = [self isImageBlurry:image];
-  
-  id objects[] = { isImageBlurryResult ? @YES : @NO };
-  NSUInteger count = sizeof(objects) / sizeof(id);
-  NSArray *dataArray = [NSArray arrayWithObjects:objects
-                                           count:count];
-  
-  callback(@[[NSNull null], dataArray]);
-}
-
 RCT_EXPORT_METHOD(concatenateHorizontally:(NSArray *)imagesAsBase64 callback:(RCTResponseSenderBlock)callback) {
   UIImage* image1 = [self decodeBase64ToImage:imagesAsBase64[0]];
   UIImage* image2 = [self decodeBase64ToImage:imagesAsBase64[1]];
@@ -64,7 +52,7 @@ RCT_EXPORT_METHOD(concatenateVertically:(NSArray *)imagesAsBase64 callback:(RCTR
 RCT_EXPORT_METHOD(changeImageContrast:(NSString *)imageAsBase64 alpha:(double)alpha callback:(RCTResponseSenderBlock)callback) {
   UIImage* image = [self decodeBase64ToImage:imageAsBase64];
   
-  cv::Mat matImage = [self convertUIImageToCVMat:image];
+  cv::Mat matImage = [self cvMatFromUIImage:image];
 
   cv::Mat new_matImage = cv::Mat::zeros(matImage.size(), matImage.type());
 
@@ -79,7 +67,6 @@ RCT_EXPORT_METHOD(changeImageContrast:(NSString *)imageAsBase64 alpha:(double)al
   UIImage* new_imageUI = [self UIImageFromCVMat:new_matImage];
   
   NSString* dstBase64 = [self encodeToBase64String:new_imageUI];
-  // NSString* dstBase64 = @"Hello from Native C0de!!!";
   
   id object = { dstBase64 };
   NSArray *array = [NSArray arrayWithObject:object];
@@ -171,60 +158,6 @@ RCT_EXPORT_METHOD(changeImageContrast:(NSString *)imageAsBase64 alpha:(double)al
 
 - (NSString *)encodeToBase64String:(UIImage *)image {
   return [UIImagePNGRepresentation(image) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
-}
-
-- (cv::Mat)convertUIImageToCVMat:(UIImage *)image {
-  CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
-  CGFloat cols = image.size.width;
-  CGFloat rows = image.size.height;
-  
-  cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
-  
-  CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
-                                                  cols,                       // Width of bitmap
-                                                  rows,                       // Height of bitmap
-                                                  8,                          // Bits per component
-                                                  cvMat.step[0],              // Bytes per row
-                                                  colorSpace,                 // Colorspace
-                                                  kCGImageAlphaNoneSkipLast |
-                                                  kCGBitmapByteOrderDefault); // Bitmap info flags
-  
-  CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
-  CGContextRelease(contextRef);
-  
-  return cvMat;
-}
-
-- (BOOL) isImageBlurry:(UIImage *) image {
-  // converting UIImage to OpenCV format - Mat
-  cv::Mat matImage = [self convertUIImageToCVMat:image];
-  cv::Mat matImageGrey;
-  // converting image's color space (RGB) to grayscale
-  cv::cvtColor(matImage, matImageGrey, CV_BGR2GRAY);
-  
-  cv::Mat dst2 = [self convertUIImageToCVMat:image];
-  cv::Mat laplacianImage;
-  dst2.convertTo(laplacianImage, CV_8UC1);
-  
-  // applying Laplacian operator to the image
-  cv::Laplacian(matImageGrey, laplacianImage, CV_8U);
-  cv::Mat laplacianImage8bit;
-  laplacianImage.convertTo(laplacianImage8bit, CV_8UC1);
-  
-  unsigned char *pixels = laplacianImage8bit.data;
-  
-  // 16777216 = 256*256*256
-  int maxLap = -16777216;
-  for (int i = 0; i < ( laplacianImage8bit.elemSize()*laplacianImage8bit.total()); i++) {
-    if (pixels[i] > maxLap) {
-      maxLap = pixels[i];
-    }
-  }
-  // one of the main parameters here: threshold sets the sensitivity for the blur check
-  // smaller number = less sensitive; default = 180
-  int threshold = 180;
-  
-  return (maxLap <= threshold);
 }
 
 @end
